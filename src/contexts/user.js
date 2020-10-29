5c204554a41df044b5f93376a0213f66574feb1f
Gabriel Carneiro <gabriel.carneiro.castro@gmail.com>
2020-10-29 15:03:11 -0300

    adicionei o userDb no UserContext

--- src/contexts/user.js
+++ src/contexts/user.js
@@ -6,17 +6,34 @@ export const UserContext = React.createContext();
 function UserProvider(props) {
   const [firebaseUser, setFirebaseUser] = useState(firebase.auth().currentUser);
   const [isAuth, setIsAuth] = useState(!!firebase.auth().currentUser);
+  const [dbUser, setDbUser] = useState(null);
 
-  useEffect(() => firebase.auth().onAuthStateChanged(() => {
+  useEffect(() => firebase.auth().onAuthStateChanged(async () => {
+    const db = firebase.firestore();
     const { currentUser } = firebase.auth();
+
+    const usersCollection = db.collection('users');
+    const userDoc = await usersCollection.doc(currentUser.uid).get();
+    const userDocData = userDoc.data();
+
+    if (!userDocData) {
+      const newDbUser = {
+        pacientes: [],
+      };
+      await usersCollection.doc(currentUser.uid).set(newDbUser);
+      setDbUser(newDbUser);
+    } else {
+      setDbUser(userDocData);
+    }
+
     setFirebaseUser(currentUser);
     setIsAuth(!!currentUser);
   }), []);
 
-
   const state = {
     isAuth,
     firebaseUser,
+    dbUser,
   };
 
   return (
